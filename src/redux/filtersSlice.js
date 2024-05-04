b0da6531c39fea3ccc76f703740417a9b2265f4b
Anton <antonklim6@gmail.com>
2024-05-04 15:09:30 +0200

    delete trash

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,7 +1,3 @@
-// export const getContacts = (state) => state.contacts
-
-// export const getStatusFilter = (state) => state.contacts.status;
-
 export const filtersSlice = (name) => {
     return {
         type: "filters/filtersSlice",
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,19 +2,6 @@ import { createStore, combineReducers } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
-// const initialState = {
-//     contacts: {
-// 		items: [
-//             {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//             {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//             {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//             {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-//           ]
-// 	},
-        // filters: {
-        //         name: ""
-        // }
-// }
 
 const rootReducer = combineReducers({
         contacts: contactsReducer,
